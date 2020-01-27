25504845bc5b57268f87bb8917877e75724e59a9
Gilmanov Garri <3434455@mail.ru>
2020-01-27 11:47:39 +0500

    heroku1

---
+++ tasks/models.py
@@ -0,0 +1,53 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.urls import reverse
+
+
+class Category(models.Model):
+    slug = models.CharField(max_length=128)
+    name = models.CharField(max_length=256)
+    todos_count = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return f'{self.name} ({self.slug})'
+
+class Priority(models.Model):
+    name = models.CharField(max_length=50)
+    todos_count = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        verbose_name = 'Приоритет'
+        verbose_name_plural = 'Приоритеты'
+
+    def __str__(self):
+        return self.name
+
+class TodoItem(models.Model):
+
+    description = models.TextField("описание")
+    is_completed = models.BooleanField("выполнено", default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="tasks"
+    )
+    priority = models.ForeignKey(
+        Priority, on_delete=models.CASCADE, related_name="todo"
+    )
+    category = models.ManyToManyField(Category, blank=True)
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+
+    def __str__(self):
+        return self.description.lower()
+
+    def get_absolute_url(self):
+        return reverse("tasks:details", args=[self.pk])
+
